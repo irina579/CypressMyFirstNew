4ec06c4e5a78b8e8eb1ee2c4baa816a24c96fe60
Irina <i.bobrovskaya@belitsoft.com>
2023-02-21 01:24:43 +0300

    Show Ones test progress

--- cypress/e2e/DashTests/DASH_smoke_ones.cy.js
+++ cypress/e2e/DashTests/DASH_smoke_ones.cy.js
@@ -16,6 +16,9 @@ describe("DASH smoke tests",
   cy.get(".main-heading").click()  
   cy.contains("Apply").click()
 }
+  function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+  } 
   beforeEach(() => {
    // testLog()  
     cy.visit(Cypress.env('url_g'))
@@ -444,6 +447,176 @@ describe("DASH smoke tests",
       })
 
     })
+    context("Ones Unit - Show Ones grid", ()=>{
+      beforeEach(() => {
+        cy.contains('.link__title','Show Ones').click()
+        cy.url().should('include', '/ones/show')
+        //cy.contains('.Vheader-text',"Site").next().click()
+        //cy.contains('.option-group__label',Cypress.env("bu")).next().find("[value="+Cypress.env("site_id")+"]").click()
+      }) 
+     it.only("Can open Show Ones => Ones grid", () => {
+       // cy.get('div>.filter-view-current').its('length').then((Filter) => {
+        
+        
+        cy.get('.Vheader__form').then(($body) => {   
+          if ($body.find('div>.filter-view-current').length>0){ //check if default custom filter exists
+          cy.contains("to see Ones content").should("not.exist")
+          cy.get('div>.filter-view-current__delete').click()
+          cy.log('Clear default filter')
+          }
+        })
+        cy.contains("to see Ones content").should("exist")
+        cy.contains('.btn__overflow','Select show').click() //selecting a show in dropdown containing current year    
+        let date = new Date().getFullYear()
+        ///cy.get('.search__wrapper>input').eq(0).type(date)
+        //cy.get('.search__wrapper>input').eq(0).clear()
+        cy.get('.VSelect__search').eq(0).parent(1).then(($Filter) => {
+          if($Filter.find(date).length>0) {
+            cy.contains('li',date).eq(0).click()           
+           // cy.contains('a','13L | 29 Mar 2021 - 20 Dec 2021').click()
+           //cy.get('[value="961"]').eq(0).click()
+          }
+          else{
+            cy.get('.VSelect__search').eq(0).parent(1).find('li').eq(getRandomInt(10)).click() 
+          }
+          })
 
+        cy.get('[data-content="Select a discipline"]').parent().next(1).click()
+        // cy.contains(".btn__overflow","File").should("exist") //checks if File button available
+        // cy.contains('.Vheader-text',"Dates").prev().click()
+        // cy.contains("Select All").click()
+        // cy.contains("label", Cypress.env('IDL_dept')).click() //checks 1 department
+        // cy.get(".main-heading").click()
+        // cy.contains("Apply").click()
+        // //checks if any artist comes from BE
+        // cy.intercept('/api/idldepartmentonesnew/GetDepartmentOnes').as('grid_list')
+        // cy.contains("Apply").click()
+        // cy.wait('@grid_list').then(({response}) => {
+        //   expect(response.statusCode).to.eq(200)
+        //   let artist_count=response.body.reference.artistPositions.items.length
+        //   let FirstName
+        //   if(artist_count>0){
+        //   FirstName=response.body.reference.artistPositions.items[1].name.username
+        //   cy.get(".item_artist",{setTimeout: `${Cypress.env('elem_timeout')}`}).eq(0).should("exist")
+        //   cy.contains(".item__info__name",FirstName).eq(0).should("exist") //check if 1-st artist exists in table and matches response
+        //   cy.log("The first artist is - "+FirstName)
+        //   cy.contains(".item__info__department-name",Cypress.env('IDL_dept')).should("exist")
+        //   artist_count=artist_count-1
+        //   }
+        //   cy.log("The number of artist came from BE - "+artist_count)
+        // })
+        // cy.contains(".btn__overflow","File").should("exist") //checks if File button available
+      })
+      it("Can open IDL Teams", () => {
+        cy.contains(".tab-title", "Teams").click()
+        SelectAllDepts() //checks all departments
+        cy.get('.TeamsTab__search').should('exist').type("123456789")
+        cy.get('.VComboSearch__clear').click()
+        cy.get('body').then(($body) => {   
+            if ($body.find('.name__team').length>0){ //check if any team exists
+              cy.log("The number of existing teams - "+$body.find('.name__team').length)
+              cy.get(".name__team").eq(0).should("exist")
+              cy.get('[data-icon="pencil"]').should("not.exist") 
+            }
+            else{
+              cy.log("There are NO teams. The number of existing teams - "+$body.find('.name__team').length)
+            }
+          })
+        cy.contains('.Vheader-text',"Dates").prev().click()
+        cy.contains("Select All").click()
+        cy.contains("label", Cypress.env('IDL_dept')).click() //checks 1 department
+        cy.get(".main-heading").click()
+        cy.contains("Apply").click()
+        //check API whether teams exist for this dept
+        cy.request('POST', Cypress.env('url_g')+"/api/ShotTeamsNewApi/GetShotTeams", {departmentIdCollection:[Cypress.env('IDL_dept_id')],siteId:Cypress.env('site_id'),isIdl:true}).then(
+        (response) => {
+        expect(response.status).to.eq(200) //status 200
+        expect(response.body.status).to.eq('success') //status success
+        let teams_count=response.body.reference.shotTeams.length
+        cy.log(teams_count)
+        if (teams_count>0){ //if there are teams
+        let FirstTeamName=response.body.reference.shotTeams[0].name
+          cy.contains(".name__team",FirstTeamName).should("exist")
+          cy.get('[data-icon="pencil"]').eq(0).should("exist")  
+          cy.log('The number of existing teams - '+teams_count+'. The first team name is - '+FirstTeamName)
+        }
+        else{ //if there are no teams
+          cy.get(".name__team").should("not.exist")
+          cy.get('[data-icon="pencil"]').should("not.exist")  
+          cy.log('The number of existing teams - '+teams_count)
+        } 
+      }) 
+      })
+      it("Can open IDL Manager Lab", () => {
+        cy.contains(".VTab__btn", "Manager Lab").click()
+        cy.contains(".btn__overflow","Nothing selected").click()
+        cy.contains("a",Cypress.env('IDL_dept')).click()
+        //checks if any artist comes from BE
+        cy.intercept("/api/departmentones/site/"+Cypress.env('site_id')+"/department/"+Cypress.env('IDL_dept_id')+"/dss").as('grid_list')
+        cy.log("/api/departmentones/site/"+Cypress.env('site_id')+"/department/"+Cypress.env('IDL_dept_id')+"/dss")
+        cy.contains("Apply").click()
+        cy.contains(".btn__overflow","File").should("exist") //checks if File button available
+
+        cy.wait('@grid_list',{requestTimeout:`${Cypress.env('req_timeout')}`}).then(({response}) => {
+          expect(response.statusCode).to.eq(200)
+          let artist_count=response.body.reference.artistInfos.items.length
+          let FirstName
+          cy.log("The number of artist came from BE - "+artist_count)
+          if(artist_count>0){
+          FirstName=response.body.reference.artistInfos.items[0].userName
+          cy.get(".approval-status").eq(0).should("exist") //checks there is an artist in the grid=>approval status
+          cy.contains(".cell_username",FirstName).eq(0).should("exist") //check if 1-st artist exists in table and matches response   
+          cy.log("The first artist is - "+FirstName)
+          } 
+        })
+        cy.contains(".btn__overflow","Current State").click() //open snapshot dropdown
+        let CountSnap=0
+        // further checks will go if there are any snapshots
+        cy.contains('a','Current State').parent().its('length').then((CountSnap) => {
+            cy.log("length="+CountSnap)
+            if(CountSnap>=2){
+              cy.contains('a','Current State').parent().eq(CountSnap).click()
+              //checks if any artist comes from BE
+              cy.contains("Apply").click()
+              cy.wait('@grid_list',{requestTimeout:`${Cypress.env('req_timeout')}`}).then(({response}) => {
+              expect(response.statusCode).to.eq(200)
+              let artist_count=response.body.reference.artistInfos.items.length
+              let FirstName=response.body.reference.artistInfos.items[0].userName
+              cy.log("The number of artist came from BE - "+artist_count)
+              cy.log("The first artist is - "+FirstName)
+              if(artist_count>0){
+               cy.get(".approval-status").eq(0).should("exist") //checks there is an artist in the grid=>approval status
+               cy.contains(".cell_username",FirstName).eq(0).should("exist") //check if 1-st artist exists in table and matches response   
+               cy.contains(".btn__overflow","File").should("not.exist") //checks if File button UNavailable (read only mode)
+              }
+              })     
+            }
+        })
+      })
+      it("Can open IDL Vacancies converted info", () => {
+        cy.contains(".tab-title", "Vacancies converted info").click()
+        cy.contains(".btn__overflow","Nothing selected").click()
+        cy.contains("a",Cypress.env('IDL_dept')).click()
+        cy.contains("Apply").click()
+        //check API whether there are converted artists
+        cy.request('POST', Cypress.env('url_g')+"/api/PositionConverterApi/GetConvertedPositionsHistory", {siteId:Cypress.env('site_id'),departmentIds:[Cypress.env('IDL_dept_id')]}).then(
+        (response) => {
+        expect(response.status).to.eq(200) //status 200
+        expect(response.body.status).to.eq('success') //status success
+        let converted_count=response.body.reference.length
+        cy.log(converted_count)
+        if (converted_count>0){ //if there are converted artists
+        let FirstName=response.body.reference[0].realArtistUserName
+          cy.get(".table__row").find(".row__column",FirstName).should("exist") //check if artist exists in table 
+          cy.log('The number of converted artists - '+converted_count+'. The first artist name is - '+FirstName)
+        }
+        else{ //if there are no converted artists
+          cy.get(".table__row").eq(1).should("not.exist")
+          cy.log('The number of converted artists - '+converted_count)
+        } 
+        }) 
+      })  
+      
+    })
     })
     export{}
