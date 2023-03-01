401379bd7ed2ab3d0a57e7181f99371c3e9c8614
Irina <i.bobrovskaya@belitsoft.com>
2023-03-02 00:40:45 +0300

    Users search and filters finished

--- cypress/e2e/DashTests/DASH_admin.cy.js
+++ cypress/e2e/DashTests/DASH_admin.cy.js
@@ -4,6 +4,7 @@ describe("DASH smoke tests/Managements",
   env: {
     req_timeout: 30000,
     elem_timeout: 30000,
+    user:'alex'
    // password: 'global'
   },
 },
@@ -41,7 +42,7 @@ describe("DASH smoke tests/Managements",
      
          
     })
-    it.only('Manage site permissions', () => {
+    it('Manage site permissions', () => {
         cy.contains('.link__title','Manage Sites Permissions').click()
         cy.url().should('include', '/UserPermission/Index')
         cy.contains('.btn','Save').should('have.attr', 'disabled') //verify Save disabled until changes are done
@@ -49,15 +50,82 @@ describe("DASH smoke tests/Managements",
         cy.get("[value="+Cypress.env("site_id")+"]").click() //select site mentioned in config
         cy.contains('.col-sm-6','Local Head of Workforce Planning').siblings('.col-sm-1').click()
         cy.get('ul>.select-all').find('label').click() //select all
-        cy.contains('.col-sm-6','Local Head of Workforce Planning').siblings('.col-sm-5').should('include.text',',') //verify there are selected users
+        cy.contains('.col-sm-6','Local Head of Workforce Planning').siblings('.col-sm-5').contains(/[a-z]+/)
         cy.get('ul>.select-all').find('label').click() //unselect all
-        cy.contains('.col-sm-6','Local Head of Workforce Planning').siblings('.col-sm-5').should('not.include.text',',') //verify users are removed
+        //verify users are removed
+        cy.contains('.col-sm-6','Local Head of Workforce Planning').siblings('.col-sm-5').invoke('text').then((text => {
+            expect(text.trim()).to.eq('')
+        }));
         cy.contains('.btn','Save').should('not.have.attr', 'disabled') //save button is enabled after changes
         cy.get('.main-title').click()
         cy.contains('.btn','Cancel').click() //cancel the changes
         cy.contains('.btn','Save').should('have.attr', 'disabled') //verify Save disabled without changes
     })
-    
+    context("Users", ()=>{
+        beforeEach(() => {
+            cy.contains('.link__title','Users').click()
+            cy.url().should('include', '/Admin/Users')
+        }) 
+    it('Users=> Search tab', () => {
+        cy.get('div> .search__input').type(`${Cypress.env('user')}`) //search for user
+        cy.intercept('GET', '**/api//UserPermissionApi/FindUsers?userNameOrEmail*').as('grid_list')
+        cy.contains('.btn','Apply').click()
+        cy.wait('@grid_list',{requestTimeout:`${Cypress.env('req_timeout')}`}).then(({response}) => {
+            expect(response.statusCode).to.eq(200)
+            let user_count=response.body.length
+            let FirstName
+            if(user_count>0){
+            expect(JSON.stringify(response.body[getRandomInt(user_count)]).toLowerCase(),'There is a text matching search in random user').to.contain(`${Cypress.env('user')}`)
+            FirstName=normalizeText(response.body[getRandomInt(user_count)].userName) //store any random user
+            cy.log(FirstName)
+            cy.contains('.row__column', FirstName).scrollIntoView() //scroll to this user on UI to make sure he exists
+            cy.get('.ui-checkbox_default').eq(1).scrollIntoView().click()
+            cy.contains('.btn','Delete').click() //check Delete button action
+            cy.get('.VNotification__message').should('exist')
+            cy.contains('.btn','Cancel').click()
+            cy.contains('.btn','Reset').click() //Reset search
+            cy.get('div .users-row').should('not.exist')         
+            }
+            cy.log("The number of users came from BE after search- "+user_count)
+          })
+    })
+    it.only('Users=> Filter tab', () => {
+        cy.contains('.tab-title', 'Filter').click()
+        cy.contains('.header__label', 'Entity').next('div').first().click()
+        cy.get("[value="+Cypress.env("site_id")+"]").click() //select site mentioned in config
+        cy.get("[value="+Cypress.env("site_id")+"]").find('a').invoke('text').then((site => {
+        cy.contains('.header__label', 'Group of permissions').next('div').first().click()
+
+        let random_permission=getRandomInt(10)+1
+        cy.get("[value="+random_permission+"]").click() //select site mentioned in config
+        cy.get("[value="+random_permission+"]").find('a').invoke('text').then((permission => {
+            //expect(text.trim()).to.eq('')
+            cy.log(permission)
+            cy.intercept('GET', '**/api/UserPermissionApi/GetUsersByFilter?entityId*').as('grid_list') 
+            cy.contains('.btn','Apply').click()
+            cy.wait('@grid_list',{requestTimeout:`${Cypress.env('req_timeout')}`}).then(({response}) => {
+                    expect(response.statusCode).to.eq(200)
+                    let user_count=response.body.length
+                    if(user_count>0){
+                    expect(JSON.stringify(response.body[getRandomInt(user_count)]).toLowerCase(),'There is a text matching filtered site').to.contain(site.toLowerCase().trim())
+                    expect(JSON.stringify(response.body[getRandomInt(user_count)]).toLowerCase(),'There is a text matching filtered permission').to.contain(permission.toLowerCase().trim())
+                    cy.get('.ui-checkbox_default').eq(1).scrollIntoView().click()
+                    cy.contains('.btn','Delete').click() //check Delete button action
+                    cy.get('.VNotification__message').should('exist')
+                    cy.contains('.btn','Cancel').click()
+                    cy.contains('.btn','Reset').click() //Reset search
+                    cy.get('div .users-row').should('not.exist')         
+                    }
+                    cy.log("The number of users came from BE after search- "+user_count)
+                  })
+        })      
+        )
+    })
+    )
+})
+})
+
+
 
 
 
