c3d737bde3871f63a79d76dc864ddf0aca77746e
Irina <i.bobrovskaya@belitsoft.com>
2023-03-28 15:46:25 +0300

    test

--- cypress/e2e/DashTests/quick_check.cy.js
+++ cypress/e2e/DashTests/quick_check.cy.js
@@ -20,7 +20,7 @@ describe('DASH login', () => {
  
      
    })
-   it.only('Random test', () => {
+   it.only('Random value test', () => {
     let random_search=Math.random().toString(36).substring(2,4)
     cy.log(random_search)
  
