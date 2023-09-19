9ad4427550c8b2d26f3bbe47429e85ede176b1f0
Irina <i.bobrovskaya@belitsoft.com>
2023-09-19 14:19:01 +0300

    Small fix in quick check

--- cypress/e2e/DashTests/quick_check.cy.js
+++ cypress/e2e/DashTests/quick_check.cy.js
@@ -2,7 +2,7 @@ describe('DASH login', () => {
    //cy.get('[data-test=new-todo]')
    //cy.get('[data-icon="pencil"]')// 
   let random_search=Math.random().toString(36).substring(2,4)
-  it.only('Random value test', () => {
+  it.only('Random value test - simpe check', () => {
     cy.log(random_search)
     if(true){
       random_search="changed"
