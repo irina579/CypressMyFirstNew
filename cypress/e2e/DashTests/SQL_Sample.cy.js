69448a3386346716547943ca6c04cc1b2d72cd62
Irina Bobrovskaya <i.bobrovskaya@belitsoft.com>
2024-12-05 13:51:47 +0300

    Added DB test sample

---
+++ cypress/e2e/DashTests/SQL_Sample.cy.js
@@ -0,0 +1,45 @@
+describe('Dynamic DB Query Test', () => {
+  it('Check if the third record has UserName "kate-mu"', () => {
+    const query = 'SELECT TOP 5 UserId, UserName FROM dbo.UserProfile';
+
+    cy.task('queryDb', query).then((result) => {
+      expect(result).to.have.length.greaterThan(2); // Ensure at least 3 records
+
+      const thirdRecord = result[2]; // Get the third record (index 2)
+
+      // Clean up the UserName by trimming and replacing &nbsp; with an empty string
+      const cleanedUserName = thirdRecord.UserName.trim().replace(/&nbsp;/g, '');
+
+      expect(cleanedUserName).to.equal('kate-mu'); // Assert UserName is 'kate-mu'
+      expect(thirdRecord.UserId).to.be.a('number'); // Ensure UserId is a number
+    });
+  });
+  it('Check if feature=18 is enabled for MPC BU in Config.BusinessUnitFeatures table', () => {
+    //const query = "Select * from Config.BusinessUnitFeatures where BusinessUnitId in (select EntityId from dbo.entities where Name='MPC' and Deleted=0)";
+    const query = "Select * from Config.BusinessUnitFeatures where BusinessUnitId in (select EntityId from dbo.entities where Name="+"'"+Cypress.env('bu')+"'"+" and Deleted=0)";
+
+    Cypress.env('bu')
+    cy.task('queryDb', query).then((result) => {
+      // Ensure at least 3 records exist
+      expect(result).to.have.length.greaterThan(1);
+  
+      // Check if any record has FeatureId = 18
+      const hasFeatureId18 = result.some(item => item.FeatureId === 18);
+      expect(hasFeatureId18).to.be.true; // Assert that at least one record has FeatureId = 18
+  
+      // Verify other assertions
+      const thirdRecord = result[1]; // Get the second record (index 1)
+      expect(thirdRecord.FeatureId).to.equal(19); // Example assertion for FeatureId
+      expect(thirdRecord.FeatureId).to.be.a('number'); // Ensure FeatureId is a number
+    });
+  });
+  
+});
+
+
+
+
+
+
+
+
